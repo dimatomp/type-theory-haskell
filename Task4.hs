34c8e9eab7c3b430d0526a92da779db91c48226f
Dmitry Tomp <dmitrytomp@gmail.com>
2015-06-24 13:48:30 +0300

    Added memoization

--- Task4.hs
+++ Task4.hs
@@ -1,10 +1,13 @@
-{-# LANGUAGE DeriveDataTypeable #-}
-
 import Control.Applicative
 import Control.Lens
 import Control.Monad
+import Control.Monad.Reader
+import Control.Monad.State
+import Control.Monad.ST
 
 import Data.Char
+import Data.Hashable
+import Data.HashTable.ST.Basic as H
 import Data.List
 import Data.Maybe
 
@@ -20,6 +23,11 @@ instance Show DeBrujin where
     show (DApp f s) = '(':show f ++ ' ':show s ++ ")"
     show (DLam par) = '\\':show par
 
+instance Hashable DeBrujin where
+    hashWithSalt p (DVar n) = hashWithSalt p n
+    hashWithSalt p (DApp f s) = (((997 * p + hashWithSalt p f) * p + 1009) * p + hashWithSalt p s) * p + 1013
+    hashWithSalt p (DLam v) = 1019 * p + hashWithSalt p v
+
 dFreeVars :: Simple Traversal DeBrujin Int
 dFreeVars = apply 0
     where
@@ -29,17 +37,31 @@ dFreeVars = apply 0
         apply n f (DApp fi se) = DApp <$> apply n f fi <*> apply n f se
         apply n f (DLam v) = DLam <$> apply (n + 1) f v
 
-dbReduce :: DeBrujin -> DeBrujin
-dbReduce (DApp (DLam pat) sub) = subst 1 pat sub & dFreeVars -~ 1 & dbReduce
+type Memoize s = ReaderT (HashTable s DeBrujin DeBrujin) (ST s)
+
+checkAvail :: DeBrujin -> Memoize s DeBrujin -> Memoize s DeBrujin
+checkAvail ex mem = do
+    table <- ask
+    res <- lift $ H.lookup table ex
+    case res of
+        Just already -> return already
+        Nothing -> do what <- mem
+                      lift $ H.insert table ex what
+                      return what
+
+dbReduce :: DeBrujin -> Memoize s DeBrujin
+dbReduce expr@(DApp (DLam pat) sub) = checkAvail expr $ dbReduce $ subst 1 pat sub & dFreeVars -~ 1
     where
         subst n (DVar m) sub = if m == n then sub & dFreeVars +~ n else DVar m
         subst n (DApp f s) sub = subst n f sub `DApp` subst n s sub
         subst n (DLam v) sub = DLam $ subst (n + 1) v sub
-dbReduce expr@(DApp f s) = case dbReduce f of
-    DLam pat -> dbReduce $ DApp (DLam pat) s
-    first    -> DApp first $ dbReduce s
-dbReduce (DVar n) = DVar n
-dbReduce (DLam v) = DLam $ dbReduce v
+dbReduce expr@(DApp f s) = checkAvail expr $ do
+    first <- dbReduce f
+    case first of
+        DLam pat -> dbReduce $ DApp (DLam pat) s
+        _        -> liftM (DApp first) $ dbReduce s
+dbReduce (DVar n) = return $ DVar n
+dbReduce expr@(DLam v) = checkAvail expr $ liftM DLam $ dbReduce v
 
 chBr :: LambdaExpr -> DeBrujin
 chBr expr = convert (expr ^.. coerced freeVars.name) expr
@@ -67,4 +89,4 @@ churchBrujin = iso chBr brCh
 
 main = do
     expr <- liftM parseLambda $ readFile "task4.in"
-    writeFile "task4.out" $ show $ over churchBrujin dbReduce expr
+    writeFile "task4.out" $ show $ runST $ new >>= runReaderT (churchBrujin dbReduce expr)
